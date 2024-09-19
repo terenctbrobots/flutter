9df06b9afa7b4bdd64ac11ee1f9c11a5defd5f19
Terence Tan <terence.tan@bigbadrobots.com>
2024-09-19 11:03:37 +0800

    Add styling

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -18,10 +18,17 @@ class GradientContainer extends StatelessWidget {
           end: Alignment.bottomRight,
         ),
       ),
-      child: Column(children: [
-        Image.asset('assets/images/dice-2.png', width: 200),
-        TextButton(onPressed: rollDice, child: const Text("Roll Dice"))
-      ]),
+      child: Center(
+        child: Column(
+          mainAxisSize:MainAxisSize.min,
+          children: [
+          Image.asset('assets/images/dice-2.png', width: 200),
+          TextButton(
+            onPressed: rollDice, 
+            style: TextButton.styleFrom(foregroundColor: Colors.white,textStyle: const TextStyle(fontSize:28)),
+            child: const Text("Roll Dice"),)
+        ]),
+      ),
     );
   }
 }
