f820c6b0305bdae3d20fa031ec0b3d51cce1e933
Terence Tan <terence.tan@bigbadrobots.com>
2024-09-26 16:16:18 +0800

    Finish dice app

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,34 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/dice_roller.dart';
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(this.firstColor, this.secondColor, {super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
 
-  final Color firstColor;
-  final Color secondColor;
-
-  void rollDice() {}
+  final Color color1;
+  final Color color2;
 
   @override
   Widget build(context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [firstColor, secondColor],
+          colors: [color1, color2],
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize:MainAxisSize.min,
-          children: [
-          Image.asset('assets/images/dice-2.png', width: 200),
-          TextButton(
-            onPressed: rollDice, 
-            style: TextButton.styleFrom(foregroundColor: Colors.white,textStyle: const TextStyle(fontSize:28)),
-            child: const Text("Roll Dice"),)
-        ]),
-      ),
+      child: const Center(child: DiceRoller()),
     );
   }
 }
--- first_app/lib/styled_text.dart
+++ first_app/lib/styled_text.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 
 class StyledText extends StatelessWidget {
-  const StyledText (this.text,{super.key});
-  
+  const StyledText(this.text, {super.key});
+
   final String text;
 
   @override
   Widget build(context) {
-    return  Center(
-            child: Text(text,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 28,
-                ),
-                ),
-          );
+    return Center(
+      child: Text(
+        text,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 28,
+        ),
+      ),
+    );
   }
 }
